5086c1d763a9e3242ab8d9e4ccb9a634b9cde931
Younes Serraj <younes.serraj@gmail.com>
2013-03-29 00:11:54 +0100

    fixed model feeds (update entries)

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -24,27 +24,20 @@ class Feed < ActiveRecord::Base
   end
 
   def update_entries
-    lastentry = self.entries.last
-    if lastentry.nil?
-      get_entries
-    else
-      feed = Feedzirra::Feed.fetch_and_parse(self.feed_url, { if_modified_since: self.last_modified })
-      if feed.is_a?(Integer) == false and feed.last_modified != self.last_modified
-        feed.entries.each do |e|
-          if e.published > lastentry.published
-            entry = Entry.new
-            entry.feed_id = self.id
-            entry.title = e.title
-            entry.url = e.url
-            entry.author = e.author
-            entry.summary = e.summary
-            entry.content = e.content
-            entry.published = e.published
-            entry.categories = e.categories
-            if entry.save
-              self.unread += 1
-            end
-          end
+    feed = Feedzirra::Feed.fetch_and_parse(self.feed_url, { if_modified_since: self.last_modified })
+    if feed.is_a?(Integer) == false and feed.last_modified != self.last_modified
+      feed.entries.each do |e|
+        entry = Entry.new
+        entry.feed_id = self.id
+        entry.title = e.title
+        entry.url = e.url
+        entry.author = e.author
+        entry.summary = e.summary
+        entry.content = e.content
+        entry.published = e.published
+        entry.categories = e.categories
+        if entry.save
+          self.unread += 1
         end
       end
       self.save
